54e0a05fc8cd944b07a9f6ab24ea17a8320e7a36
Jared Frazier <cscidev001@gmail.com>
2023-01-11 12:41:16 +0100

    initialize some arrays and print them out

--- proto/MPI_reduce_arrays.c
+++ proto/MPI_reduce_arrays.c
@@ -14,6 +14,15 @@ $ prun -v -1 -np 2 -script $PRUN_ETC/prun-openmpi ./MPI_reduce_arrays
 #include <stdio.h>
 #include <mpi.h>
 
+#define SEED 0
+#define MAXELES 5
+
+void print_arr(double arr[], int rank) {
+    printf("RANK %d: ", rank);
+    for (int ele = 0; ele < MAXELES; ele++) printf("%.2lf ", arr[ele]);
+    printf("\n");
+}
+
 int main(int argc, char **argv) {
     // Init MPI
     MPI_Init(&argc, &argv);
@@ -22,8 +31,20 @@ int main(int argc, char **argv) {
     MPI_Comm_size(comm, &size);
     MPI_Comm_rank(comm, &rank);
 
-    printf("Hello %d/%d\n", rank, size);
+    //printf("Hello %d/%d\n", rank, size);
 
+    // Initialize some data
+    srand(SEED);
+    double local_array[MAXELES];
+    for (int i = 0; i < MAXELES; i++) local_array[i] = rand() % 10;
+
+    // Inspect data
+    print_arr(local_array, rank);
+
+    // Try to reduce the sum of elements and broadcast
+    
+
+    // Clean up
     MPI_Finalize();
     return 0;
 }
