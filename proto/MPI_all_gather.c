c6494313d96e6a61341ee11ec00a27f1a4c0bc6a
Jared Frazier <cscidev001@gmail.com>
2023-01-12 16:57:21 +0100

    MPI_Gather+MPI_Bcast is sufficient substitute but does require copying data

--- proto/MPI_all_gather.c
+++ proto/MPI_all_gather.c
@@ -10,12 +10,12 @@ prun -v -1 -np 2 -script $PRUN_ETC/prun-openmpi ./MPI_all_gather
 #include <stdio.h>
 #include <mpi.h>
 
-#define MAXBODIES 100
-#define BODYCT 8
+#define MAX_ELES 100
+#define CUR_ELES 6
 
 void print_arr(int arr[], int rank) {
     printf("RANK %d: ", rank+1);
-    for (int i = 0; i < BODYCT; i++)
+    for (int i = 0; i < CUR_ELES; i++)
         printf("%d ", arr[i]);
     printf("\n"); 
 }
@@ -29,13 +29,13 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &size);
 
-    int positions[MAXBODIES];
-    for (int position = 0; position < BODYCT; position++)
+    int positions[MAX_ELES];
+    for (int position = 0; position < CUR_ELES; position++)
         positions[position] = 0;
 
     // Determine bounds for computation
     int lower_bound, upper_bound;
-    int N = BODYCT;
+    int N = CUR_ELES;
     int P = size;
     lower_bound = rank*N/P;
     upper_bound = rank == P-1 ? (rank+1)*N/P + N%P : (rank+1)*N/P; 
@@ -46,6 +46,34 @@ int main(int argc, char **argv) {
 
     // Print values
     print_arr(positions, rank);
+
+    int gathered_positions[MAX_ELES];
+    MPI_Gather(
+        &positions[lower_bound], upper_bound-lower_bound, MPI_INT,
+        gathered_positions, upper_bound-lower_bound, MPI_INT, 0, comm); // receive amount might be variable
+
+    // MPI_Gather(
+    //     MPI_IN_PLACE, 0, MPI_DATATYPE_NULL,
+    //     &positions[lower_bound], upper_bound-lower_bound, MPI_INT, 0, comm); 
+
+    // MPI_Allgather(
+    //     MPI_IN_PLACE, 0, MPI_DATATYPE_NULL, 
+    //     &positions[lower_bound], upper_bound-lower_bound, MPI_INT, comm);
+
+    if (rank == 0) {
+        printf("---------\n");
+        printf("RANK %d: Gathered\n", rank+1);
+        print_arr(gathered_positions, rank);
+        //print_arr(positions, rank);
+        printf("---------\n");
+    }
+
+    MPI_Bcast(gathered_positions, CUR_ELES, MPI_INT, 0, comm);
+
+    print_arr(positions, rank);
+    print_arr(gathered_positions, rank);
+
+
 
     MPI_Finalize();
 }
