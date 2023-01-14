ff82bb843e63f6f6902c3c99096506fc92e46fc3
Jared Frazier <cscidev001@gmail.com>
2023-01-14 19:45:24 +0100

    added init testing script and verified it works as intended for the simple sanity check case

---
+++ bin/check-tests-status.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+# @brief Tell whether the files in the the tests directory pass the output
+# @details The relevant files match the pattern "*test.out"
---
+++ bin/tests.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+# @brief Performs thorough diff checks of parllel & serial output.
+# @details The following minimal tests should be verified
+# NP = {1, 2, 3, 4, 5, 6, 7, 8}
+# CPU_PER_PROC = {1, 4, 16}
+# (8, 16) -> NBODIES = Max(128, input)
+# Need to ask about this because in principle, anything lower would
+# mean that some processors do not have bodies
+
+# get cli
+NP=$1
+CPU_PER_PROC=$2
+N_BODIES=$3
+
+# Name files based on args
+REFERENCE_OUTPUT_FILE=tests/${NP}_${CPU_PER_PROC}_${N_BODIES}_MY_REF_OUTPUT
+OUTPUT_FILE=tests/${NP}_${CPU_PER_PROC}_${N_BODIES}_mynbody.test.out
+ERROR_FILE=tests/${NP}_${CPU_PER_PROC}_${N_BODIES}_mynbody.test.err
+DIFF_FILE=tests/${NP}_${CPU_PER_PROC}_${N_BODIES}_mynbody.test.diff
+
+# Compute sequential outputs if none available
+if test ! -f $REFERENCE_OUTPUT_FILE;
+then
+    prun -v -1 -np 1 -script $PRUN_ETC/prun-openmpi nbody/nbody-seq $N_BODIES 0 nbody.ppm 100000 | tee $REFERENCE_OUTPUT_FILE
+fi
+
+# Get outputs of parallel program
+prun -v -$CPU_PER_PROC -np $NP -sge-script $PRUN_ETC/prun-openmpi nbody/nbody-par $N_BODIES 0 nbody.ppm 100000 2> $ERROR_FILE | tee $OUTPUT_FILE
+
+# Get difference in outputs
+diff $REFERENCE_OUTPUT_FILE $OUTPUT_FILE
+
+# test differences
+if test -s $DIFF_FILE;
+then 
+    echo "Status=FAILED" >> $OUTPUT_FILE
+else 
+    echo "STATUS=PASSED" >> $OUTPUT_FILE;
+fi 
