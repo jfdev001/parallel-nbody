a21913c1bdf556b264ad5b1cddf9844aeae74026
Jared Frazier <cscidev001@gmail.com>
2023-01-17 16:46:00 +0100

    sequential expermients can now be run with either original implementation or my paralle implementation but with a single core... notably, the parallel implementation takes abuot twice as long with a single core compared to the original implementation

--- bin/sequential-experiments.sh
+++ bin/sequential-experiments.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
-# @brief Run experiments using sequential script
+# @brief Run experiments using sequential processing
 
 OUTPUT=$1
+ORIGINAL_SERIAL=$2  # 1 for original serial program, 0 for my parallel prog with -np 1
 
-if [ "$OUTPUT" == "" ]
+if [ "$OUTPUT" == "" ] || [ "$ORIGINAL_SERIAL" == "" ]
 then 
-    echo "usage: ./bin/sequential-experiments.sh OUTPUT"
+    echo "usage: ./bin/sequential-experiments.sh <name of output file> [0,1]"
     exit 1
 fi 
 
@@ -13,6 +14,11 @@ for replicate in 1 2 3
 do 
     for N_BODIES in 512 1024 4096 10000
     do 
-        prun -v -1 -np 1 -script $PRUN_ETC/prun-openmpi nbody/nbody-seq $N_BODIES 0 nbody.ppm 100 --run-xps >> $OUTPUT
+        if [ $ORIGINAL_SERIAL = 1 ]
+        then 
+            prun -v -1 -np 1 -script $PRUN_ETC/prun-openmpi nbody/nbody-seq $N_BODIES 0 nbody.ppm 100 --run-xps >> $OUTPUT
+        else 
+            prun -v -1 -np 1 -script $PRUN_ETC/prun-openmpi nbody/nbody-par $N_BODIES 0 nbody.ppm 100 --run-xps >> $OUTPUT
+        fi 
     done 
 done
