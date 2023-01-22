656625bcb6bad24ca8db8bf2fd56e0aba3791410
Jared Frazier <cscidev001@gmail.com>
2023-01-22 13:33:59 +0100

    created the tar file and re-ran tests for solidarity

--- proto/redirect_stdout.c
+++
@@ -1,8 +0,0 @@
-// mpicc -o redirect_stdout redirect_stdout.c; prun -v -1 -np 1 -script $PRUN_ETC/prun-openmpi ./redirect_stdout
-#include <stdio.h>
-
-int main() {
-    fprintf(stderr, "fprintf to stderr\n");
-    printf("To std out\n");
-    return 0;
-}
