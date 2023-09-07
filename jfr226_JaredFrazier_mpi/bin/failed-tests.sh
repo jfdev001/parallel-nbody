8fd109337a163ee0607bd9c2bbe0d2b94da990a5
Jared Frazier <cscidev001@gmail.com>
2023-09-07 12:40:33 +0200

    prepping for publish

--- jfr226_JaredFrazier_mpi/bin/failed-tests.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/bash
-# @brief Print the file names in the dir for which status of tests fails
-for file in tests/*_mynbody.test.out;
-do
-    grep "STATUS=FAILED" $file && echo $file;
-done;
